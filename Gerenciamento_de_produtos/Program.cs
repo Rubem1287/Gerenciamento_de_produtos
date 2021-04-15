0a9dda227fb87f38a739895f790fe05c18770af4
Rubem Silva <rubem.edu@outlook.com>
2021-04-15 19:57:13 -0300

    Correções simples feitas

--- Gerenciamento_de_produtos/Program.cs
+++ Gerenciamento_de_produtos/Program.cs
@@ -8,8 +8,7 @@ namespace Gerenciamento_de_produtos
     {
         static void Main(string[] args)
         {
-            List <Produto> produto = new List<Produto>();
-        ;
+            List<Produto> produtos = new List<Produto>();
 
             Console.Write("Entre com o número de produtos : ");
             int numeroProduct = int.Parse(Console.ReadLine());
@@ -28,7 +27,7 @@ namespace Gerenciamento_de_produtos
                         Console.Write("Preço : ");
                         double preco_Produto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                        produto.Add(new Produto(nome_Produto, preco_Produto));
+                        produtos.Add(new Produto(nome_Produto, preco_Produto));
                        
                         
                         break;
@@ -41,7 +40,7 @@ namespace Gerenciamento_de_produtos
                         Console.Write("Data de Fabricação (DD/MM/AAAA) : ");
                         DateTime data_Fabricacao = DateTime.Parse(Console.ReadLine());
 
-                        produto.Add(new Produto_Usado(nome_Produto_Usado, preco_Produto_Usado, data_Fabricacao));
+                        produtos.Add(new Produto_Usado(nome_Produto_Usado, preco_Produto_Usado, data_Fabricacao));
 
                        
                         
@@ -55,7 +54,7 @@ namespace Gerenciamento_de_produtos
                         Console.Write("Taxa da alfândega : ");
                         double taxa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-                        produto.Add(new Produto_Importado(nome_Produto_Importado, preco_Produto_Importado, taxa));
+                        produtos.Add(new Produto_Importado(nome_Produto_Importado, preco_Produto_Importado, taxa));
                         break;
 
 
@@ -63,9 +62,9 @@ namespace Gerenciamento_de_produtos
             }
             Console.WriteLine();
             Console.WriteLine("Preço das etiquetas");
-            foreach(Produto produtos in produto)  // (Tipo variável in lista)
+            foreach(Produto produto in produtos)  // (Tipo variável in lista)
             {
-                Console.WriteLine(produtos.Etiqueta_Produto());
+                Console.WriteLine(produto.Etiqueta_Produto());
 
             }
        
